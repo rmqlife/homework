b900b44da6fc2388ef22841c0777645227124565
rmqlife <rmqlife@gmail.com>
2017-09-25 13:00:39 -0400

    ver 0.2

---
+++ build.sh
@@ -0,0 +1,2 @@
+gcc pa1.c -o pa1 -lm
+./pa1
--- pa1.c
+++ pa1.c
@@ -7,27 +7,25 @@ typedef struct{
 	double y;
 } vec2;
 
-int set_vec2(vec2 v,double x1,double x2){
+vec2 set_vec2(double x1,double x2){
+	vec2 v;
 	v.x = x1;
 	v.y = x2;
-	return 0;
+	return v;
 }
 
 vec2 add_vec2(vec2 v1,vec2 v2){
-	vec2 result;
-	set_vec2(result,v1.x+v2.x,v1.y+v2.y);
+	vec2 result = set_vec2(v1.x+v2.x,v1.y+v2.y);
 	return result;
 }
 
 vec2 minus_vec2(vec2 v){
-	vec2 result;
-	set_vec2(result,-v.x,-v.y);
+	vec2 result = set_vec2(-v.x,-v.y);
 	return result;
 }
 
 vec2 times_vec2(double a, vec2 v){
-	vec2 result;
-	set_vec2(result, v.x*a, v.y*a);
+	vec2 result = set_vec2(v.x*a, v.y*a);
 	return result;
 }
 
@@ -43,27 +41,20 @@ double dist_vec2(vec2 a,vec2 b){
 	return length_vec2(add_vec2(a,minus_vec2(b)));
 }
 
-
-
-
 typedef struct{
 	vec2 vel,pos,acc;
 	double m;
 } body;
 
 
-
-int set_body(body b, double x0,double x1,double x2,double x3,double x4){
+body set_body(double x0,double x1,double x2,double x3,double x4){
+	body b;
 	b.m = x0;
-	set_vec2(b.pos,x1,x2);
-	set_vec2(b.vel,x3,x4);
-	set_vec2(b.acc,0,0);
-	return 0;
+	b.pos = set_vec2(x1,x2);
+	b.vel = set_vec2(x3,x4);
+	b.acc = set_vec2(0,0);
+	return b;
 }
-
-
-
-
 /*
 nbref sequential all-pairs n-body, n = 3, nts = 6325914, G = 1, DeltaT = 
 	Time = 0
@@ -90,49 +81,63 @@ nbref sequential all-pairs n-body, n = 3, nts = 6325914, G = 1, DeltaT =
 int main(){
 	body b[3];
 	int body_num = sizeof(b)/sizeof(b[0]);
-	set_body(b[0],1,-0.97000436,0.24308753,-0.46620368,-0.43236573);
-	set_body(b[1],1,0.97000436,-0.24308753,-0.46620368,-0.43236573);
-	set_body(b[2],1,0,0,0.93240737,0.86473146);
+	b[0] = set_body(1,-0.97000436,0.24308753,-0.46620368,-0.43236573);
+	b[1] = set_body(1,0.97000436,-0.24308753,-0.46620368,-0.43236573);
+	b[2] = set_body(1,0,0,0.93240737,0.86473146);
 	double DeltaT = 1e-06;
 	double G = 1;
-
-    for (int i=0;i<3;i++){
+	
+	for (int j=0;j<body_num;j++){
+		printf("b %i: ",j);
+		printf(" vel: "); print_vec2(b[j].vel);
+		printf(" pos: "); print_vec2(b[j].pos);
+		printf(" mass: %f", b[j].m);
+		printf("\n");
+	}
+	
+    for (int i=0;i<1000000;i++){
 		clock_t begin = clock();
 		
 		/* clear accs */
 		for (int j=0;j<body_num;j++)
-			set_vec2(b[j].acc,0,0);
+			b[j].acc = set_vec2(0,0);
 			
 		/* compute acc */
 		for (int j=0;j<body_num;j++)
 			for (int k=0;k<body_num;k++){
+				if (j == k)
+					continue;
+				// not sure the order!!!!
 				vec2 r = add_vec2(b[j].pos, minus_vec2(b[k].pos));
+				//print_vec2(r);
 				double r_length = length_vec2(r);
-				double temp = G*b[j].m*b[k].m/r_length/r_length/r_length;
-				
+				if (r_length<0.000001)
+					continue;
+				double temp = G*b[j].m*b[k].m/r_length/r_length/r_length;				
 				b[j].acc = add_vec2(b[j].acc, times_vec2(temp, r));
 			}		
 		
 		/* update vel,pos*/
 		for (int j=0;j<body_num;j++){
+			//printf("acc:"); print_vec2(b[j].acc); printf("\n");
 			b[j].vel = add_vec2(b[j].vel, times_vec2(DeltaT, b[j].acc));
 			b[j].pos = add_vec2(b[j].pos, times_vec2(DeltaT, b[j].vel));
 		}
 		
-		printf("time step %i \n",i); 
+		
+		printf("time %f \n",(double) i*DeltaT); 
 		/*print each velocities*/
 		for (int j=0;j<body_num;j++){
 			printf("b %i: ",j);
-			printf("vel: "); print_vec2(b[j].vel);
-			printf("pos: "); print_vec2(b[j].pos);
+			printf(" vel: "); print_vec2(b[j].vel);
+			printf(" pos: "); print_vec2(b[j].pos);
 			printf("\n");
 		}
 		
 		clock_t end = clock();
 		double time_spent = (double)(end - begin);
-	    printf("time spent %f\n", time_spent);
+	    //printf("time spent %f\n", time_spent);
 
-	    //update pos
 	}
     return 0;
 }
