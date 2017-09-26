eb0a3dc76dee78fb8c870dab22d3860e451090be
rmqlife <rmqlife@gmail.com>
2017-09-25 20:58:48 -0400

    add c99

--- compare12.sh
+++ compare12.sh
@@ -1,14 +1,14 @@
 array=(10 20 50 100 200 500 1000 2000 5000 10000)
 
 echo "method 1"
-gcc pa1_1.c -o pa1 -lm 
+gcc pa1_1.c -o pa1 -lm -std=c99
 for i in "${array[@]}"
 do
 	./pa1 $i
 done
 
 echo "method 2"
-gcc pa1.c -o pa1 -lm 
+gcc pa1.c -o pa1 -lm -std=c99
 for i in "${array[@]}"
 do
 	./pa1 $i
--- optimization.sh
+++ optimization.sh
@@ -2,42 +2,42 @@
 array=(10 100 1000)
 
 echo "none"
-gcc pa1.c -o pa1 -lm
+gcc pa1.c -o pa1 -lm -std=c99
 for i in "${array[@]}"
 do
 	./pa1 $i
 done
 
 echo "-O0"
-gcc pa1.c -o pa1 -lm -O0
+gcc pa1.c -o pa1 -lm -O0 -std=c99
 for i in "${array[@]}"
 do
 	./pa1 $i
 done
 
 echo "-O1"
-gcc pa1.c -o pa1 -lm -O1
+gcc pa1.c -o pa1 -lm -O1 -std=c99
 for i in "${array[@]}"
 do
 	./pa1 $i
 done
 
 echo "-O2"
-gcc pa1.c -o pa1 -lm -O2
+gcc pa1.c -o pa1 -lm -O2 -std=c99
 for i in "${array[@]}"
 do
 	./pa1 $i
 done
 
 echo "-O3"
-gcc pa1.c -o pa1 -lm -O3
+gcc pa1.c -o pa1 -lm -O3 -std=c99
 for i in "${array[@]}"
 do
 	./pa1 $i
 done
 
 echo "-Os"
-gcc pa1.c -o pa1 -lm -Os
+gcc pa1.c -o pa1 -lm -Os -std=c99
 for i in "${array[@]}"
 do
 	./pa1 $i
