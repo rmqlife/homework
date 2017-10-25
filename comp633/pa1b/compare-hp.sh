b22642960ae8b5d54fe43263986b1de4c0ea19cd
rmqlife <rmqlife@gmail.com>
2017-10-25 11:10:50 -0400

    update compare-hp

--- comp633/pa1b/compare-hp.sh
+++ comp633/pa1b/compare-hp.sh
@@ -5,7 +5,7 @@ run=20
 thread=(1 2 4 8 16 32 64 128 256)
 echo "running pa1b"
 rm ./pa1
-gcc pa1b-hp.c -o pa1 -lm -std=c99  -fopenmp 
+gcc pa1b-hp.c -o pa1 -lm -std=c99  -fopenmp -O3
 
 
 for i in "${array[@]}"
@@ -20,7 +20,7 @@ done
 
 echo "running pa1a"
 rm ./pa1
-gcc pa1a-hp.c -o pa1 -lm -std=c99 -fopenmp 
+gcc pa1a-hp.c -o pa1 -lm -std=c99 -fopenmp -O3
 for i in "${array[@]}"
 do
 	./pa1 $i $run $show 2
--- comp633/pa1b/pa1b-hp.c
+++ comp633/pa1b/pa1b-hp.c
@@ -78,9 +78,10 @@ int main(int argc, char *argv[]){
 			        b[j].acc = set_vec2(0,0);
 			     	    
 	            /* compute acc */
-	            #pragma omp parallel for num_threads(thread_num)
-	            for (int j=0;j<body_num;j++){
-		            for (int k=0;k<body_num;k++){
+	            int j;
+	            #pragma omp parallel for num_threads(thread_num) private(j)
+	            for (j=0;j<body_num;j++){
+		            for (int k=j+1;k<body_num;k++){
 			            // not sure the order!!!!
 			            vec2 r = add_vec2(b[k].pos, minus_vec2(b[j].pos));
 			            //print_vec2(r);
@@ -90,7 +91,7 @@ int main(int argc, char *argv[]){
 			            double temp = G*b[j].m*b[k].m/r_length/r_length/r_length;				
 			            b[j].acc = add_vec2(b[j].acc, times_vec2(temp/b[j].m, r));
 			            // newton's third law
-			            //b[k].acc = add_vec2(b[k].acc, times_vec2(temp/b[k].m, minus_vec2(r)));
+			            b[k].acc = add_vec2(b[k].acc, times_vec2(temp/b[k].m, minus_vec2(r)));
 		            }		
                 }
                 
