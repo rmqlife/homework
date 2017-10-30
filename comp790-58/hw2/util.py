d05cc70ec5e895254eb706b1c4ae5b1248687ab3
rmqlife <rmqlife@gmail.com>
2017-10-30 04:21:02 -0400

    add data

---
+++ comp790-58/hw2/util.py
@@ -0,0 +1,26 @@
+import os
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+def get_filelist(home_dir):
+    filelist = []
+    for dirpath, dirnames, filenames in os.walk(home_dir):
+        for filename in filenames:
+            fn = os.path.join(dirpath,filename)
+            filelist.append(fn)
+    filelist = sorted(filelist)
+    return filelist
+
+#extension 
+def get_filelist_ext(home_dir,ext):
+    filelist = []
+    for dirpath, dirnames, filenames in os.walk(home_dir):
+        for filename in filenames:
+            if filename.endswith(ext):
+                fn = os.path.join(dirpath, filename)
+                filelist.append(fn)
+    filelist = sorted(filelist)
+    return filelist
+
+
